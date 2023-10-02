6dbbbb06537eefc09fc8ba94d9fcea819226b35b
Ivan Ilin <ilyin1997@icloud.com>
2023-10-02 23:20:24 +0300

    add annotation config

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,13 +1,16 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class PostRepository {
     private final Map<Long, Post> posts;
     private final AtomicLong id = new AtomicLong(1);
@@ -17,7 +20,7 @@ public class PostRepository {
     }
 
     public List<Post> all() {
-        return posts.values().stream().toList();
+        return new ArrayList<>(posts.values());
     }
 
     public Optional<Post> getById(long id) {
